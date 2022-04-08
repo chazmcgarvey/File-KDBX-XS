5081062be781950fe3539f5145824d9c00c3de03
Charles McGarvey <ccm@cpan.org>
2022-04-08 16:50:42 -0600

    add badges

--- lib/File/KDBX/XS.pm
+++ lib/File/KDBX/XS.pm
@@ -22,4 +22,20 @@ There is no public interface.
 This distribution contains code from L<CryptX> and L<LibTomCrypt|https://www.libtom.net/LibTomCrypt/>,
 bundled according to their own licensing terms (which are also permissive).
 
+=begin markdown
+
+[![Linux](https://github.com/chazmcgarvey/File-KDBX-XS/actions/workflows/linux.yml/badge.svg)](https://github.com/chazmcgarvey/File-KDBX-XS/actions/workflows/linux.yml)
+[![macOS](https://github.com/chazmcgarvey/File-KDBX-XS/actions/workflows/macos.yml/badge.svg)](https://github.com/chazmcgarvey/File-KDBX-XS/actions/workflows/macos.yml)
+[![Windows](https://github.com/chazmcgarvey/File-KDBX-XS/actions/workflows/windows.yml/badge.svg)](https://github.com/chazmcgarvey/File-KDBX-XS/actions/workflows/windows.yml)
+
+=end markdown
+
+=begin HTML
+
+<a title="Linux" href="https://github.com/chazmcgarvey/File-KDBX-XS/actions/workflows/linux.yml"><img src="https://github.com/chazmcgarvey/File-KDBX-XS/actions/workflows/linux.yml/badge.svg"></a>
+<a title="macOS" href="https://github.com/chazmcgarvey/File-KDBX-XS/actions/workflows/macos.yml"><img src="https://github.com/chazmcgarvey/File-KDBX-XS/actions/workflows/macos.yml/badge.svg"></a>
+<a title="Windows" href="https://github.com/chazmcgarvey/File-KDBX-XS/actions/workflows/windows.yml"><img src="https://github.com/chazmcgarvey/File-KDBX-XS/actions/workflows/windows.yml/badge.svg"></a>
+
+=end HTML
+
 =cut
